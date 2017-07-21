8d976c88fc6b936458b90fd484f3448c6f19f500
Ishank Tandon <IshankTandon@ishank.wlan.rose-hulman.edu>
2017-07-21 15:45:28 -0400

    image uploads for featured photos

--- ImageUploads/src/app/app.component.ts
+++ ImageUploads/src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseObjectObservable } from "angularfire2/database";
+
+import * as firebase from 'firebase';
+
+interface FeaturedPhotoUrls {
+  url1?: string;
+  url2?: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  featuredPhotoStream: FirebaseObjectObservable<FeaturedPhotoUrls>;
+
+  constructor(private db: AngularFireDatabase) {
+    this.featuredPhotoStream = this.db.object('/photos/featured');
+  }
+
+  featuredPhotoSelected(event: any, photoName: string) {
+    const file: File = event.target.files[0];
+
+    const metaData = { 'contentType': file.type };
+    const storageRef: firebase.storage.Reference = firebase.storage().ref(`/photos/featured/${photoName}`);
+    const uploadTask: firebase.storage.UploadTask = storageRef.put(file, metaData);
+    console.log(`Uploading: ${file.name}`);
+
+    uploadTask.then((uploadSnapshot: firebase.storage.UploadTaskSnapshot) => {
+      console.log(`Upload is complete!`);
+      const downloadUrl = uploadSnapshot.downloadURL;
+      firebase.database().ref(`/photos/featured/${photoName}`).set(downloadUrl);
+    });
+  }
 }
